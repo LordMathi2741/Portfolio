c3c398f7456d1a7b9af04cf6e64f959a5811ade2
Mathias Jave Diaz <u202212721@upc.edu.pe>
2025-01-28 01:49:21 -0500

    refactor: changed import route paths for assets files

--- src/assets/data/en/works-en.js
+++ src/assets/data/en/works-en.js
@@ -2,31 +2,31 @@ export const worksEn = [
     {
         title: 'Report Validation',
         description: 'I developed a report validation system for transport vehicles for the company HIPERFAST SAC, with administrative functions to manage customer consumption. The project employed technologies such as C# with .Net, Vue.js, and Node.js, with a MySQL database for data persistence. A layered architecture based on Clean Architecture principles was implemented, ensuring the system’s maintainability and scalability.',
-        image: '/public/images/report-validation.png',
+        image: '/images/report-validation.png',
         url: 'http://38.25.38.94:5173/'
     },
     {
         title: 'Invoice Validation and Control',
         description: 'I developed an invoice validation and control system for the company CORSERSAC SAC, with advanced administrative features to manage invoice states and analyze consumption statistics through interactive charts. The project utilized technologies such as C# with .Net, Vue.js, and Node.js, with a MySQL database for data persistence. A layered architecture based on Clean Architecture principles was implemented, ensuring system maintainability. Additionally, software portability was achieved using Docker containers, optimizing deployment and scalability.',
-        image: '/public/images/invoice-validator.jpeg',
+        image: '/images/invoice-validator.jpeg',
         url: 'https://github.com/Corsersac-Consultant-Platform'
     },
     {
         title: 'E-commerce Platform with Personalized Recommendations',
         description: 'I developed an e-commerce platform with personalized recommendations based on users’ initial purchases and purchase history, as part of the Algorithmic Complexity course at UPC. The system includes shopping cart functionalities, general recommendations, and personalized recommendations. I used technologies such as Python, Flask, FastAPI, and MySQL for data persistence, implementing an MVC architecture on the backend to ensure maintainability and scalability. Additionally, a purchase graph was incorporated to generate recommendations using algorithms such as Dijkstra, DFS, and Quick-Union.',
-        image: '/public/images/my-favorite-product.jpeg',
+        image: '/images/my-favorite-product.jpeg',
         url: 'https://github.com/orgs/MyFavoriteProduct-Organization/repositories'
     },
     {
         title: 'Event Space Rental Application',
         description: 'I developed an event space rental application as part of the Mobile Devices Applications course at UPC. Key functionalities include reservation management using a calendar, prioritization of reservations for premium subscription users, the ability to review rented spaces, and saving spaces as favorites. I used technologies such as Flutter, C# with .Net, FireAuth for authentication with external systems, PayPal for payment management, SQLite as a local database to store favorite spaces, XUNIT for backend unit testing, and MySQL for backend data persistence. On the backend, a CQRS architecture was used alongside the DDD approach to ensure software scalability according to business needs. On the frontend, Clean Architecture was applied to guarantee the scalability of the mobile application.',
-        image: '/public/images/alquilafacil.jpeg',
+        image: '/images/alquilafacil.jpeg',
         url: 'https://github.com/orgs/Applications-for-Mobile-Devices-WX63/repositories'
     },
     {
         title: "Distance Learning Platform",
         description: "I developed an educational platform as part of EduHack 2024 sponsored by UPC. Key functionalities include course management for teachers, video conference creation, and user management by administrators. I used technologies such as Next.js, NestJs, ImgUrl as an external service for file handling, Dayli.co for video conference creation, PayPal for payment management, and Postgres for data persistence. On the backend, an MVC architecture was used to separate data persistence from business logic. Due to confidentiality agreements by the organizer, the repository and deployed application cannot be shared.",
-        image: '/public/images/handin.jpeg',
+        image: '/images/handin.jpeg',
         url: 'https://github.com/LordMathi2741?tab=repositories'
     },
     {
--- src/assets/data/es/works.js
+++ src/assets/data/es/works.js
@@ -2,37 +2,37 @@ export const works = [
     {
         title: 'Validación de reportes',
         description: 'Desarrollé un sistema de validación de reportes de vehículos de transporte para la empresa HIPERFAST SAC, con funciones administrativas para gestionar el consumo de los clientes. El proyecto empleó tecnologías como C# junto con .Net, Vue.js y Node.js, con una base de datos MySQL para la persistencia de datos. Se implementó una arquitectura en capas basada en principios de Arquitectura Limpia, asegurando la mantenibilidad y escalabilidad del sistema.',
-        image: '/public/images/report-validation.png',
+        image: '/images/report-validation.png',
         url: 'http://38.25.38.94:5173/'
     },
     {
         title: 'Validación y control de facturas',
         description: 'Desarrollé un sistema de validación y control de facturas para la empresa CORSERSAC SAC, con funcionalidades administrativas avanzadas para gestionar estados de facturas y analizar estadísticas de consumo mediante gráficos interactivos. El proyecto utilizó tecnologías como C# junto con .Net, Vue.js y Node.js, con una base de datos MySQL para la persistencia de datos. Se implementó una arquitectura en capas basada en principios de Arquitectura Limpia, lo que garantizó la mantenibilidad del sistema. Además, la portabilidad del software se logró mediante contenedores Docker, optimizando su despliegue y escalabilidad.',
-        image: '/public/images/invoice-validator.jpeg',
+        image: '/images/invoice-validator.jpeg',
         url: 'https://github.com/Corsersac-Consultant-Platform'
     },
     {
         title: 'E-commerce de productos con recomendaciones personalizadas',
         description: 'Desarrollé un sistema de e-commerce con recomendaciones personalizadas basadas en las compras iniciales y el historial de usuarios, como parte del curso de Complejidad Algorítmica en la UPC. El sistema incluye funcionalidades de carrito de compras, recomendaciones generales y personalizadas. Utilicé tecnologías como Python, Flask, FastAPI y MySQL para la persistencia de datos, implementando una arquitectura MVC en el backend para garantizar la mantenibilidad y escalabilidad. Además, incorporé un grafo de compras para generar recomendaciones mediante algoritmos como Dijkstra, DFS y Quick-Union.',
-        image: '/public/images/my-favorite-product.jpeg',
+        image: '/images/my-favorite-product.jpeg',
         url: 'https://github.com/orgs/MyFavoriteProduct-Organization/repositories'
     },
     {
         title: 'Aplicación de alquiler para espacios de eventos',
         description: 'Desarrollé una aplicación de alquiler de espacios para eventos, como parte del curso de Aplicaciones para Dispositivos Móviles en la UPC. La aplicación incluye como funcionalidades más resaltantes la gestión de reservas mediante calendario, también la priorización de reservas para usuarios con suscripción premium asi como también la posibilidad de realizar reseñas a los espacios alquilados y guardar espacios como favoritos. Utilicé tecnologías como Flutter, C# con .Net, FireAuth para la autenticación con sistemas externos, Paypal para la gestión de pagos, SQLite como base de datos local para almacenar espacios favoritos, XUNIT para realizar pruebas unitarias en el backend y MYSQL para la persistencia de datos en el backend. En el lado del backend se utilizó una arquitectura CQRS junto con el enfoque de DDD para asegurar la escalabilidad del software de acuerdo al negocio. Por otro lado en el frontend se aplicó una arquitectura limpia para garantizar la escalibilidad de la aplicación móvil',
-        image: '/public/images/alquilafacil.jpeg',
+        image: '/images/alquilafacil.jpeg',
         url: 'https://github.com/orgs/Applications-for-Mobile-Devices-WX63/repositories'
     },
     {
         title: "Plataforma de educación a distancia",
         description: "Desarrollé una plataforma educativa como parte de la EducHack 2024 auspiciada por la UPC, como parte de las funcionalidades más resaltantes se incluye la gestión de cursos para profesores, la creación de videoconferencias y la gestión de usuarios por parte de los administradores. Utilicé tecnologías como Next.js, NestJs, ImgUrl como servicio externo para manejar archivos, Dayli.co para la creación de videoconferencias, Paypal para la gestión de pagos y Postgres para la persistencia de datos. En el lado del backend se utilizó una arquitectura MVC para separar la persistencia de la lógica de negocio. Por motivos de confidencialidad por parte de la organizadora, no se puede mostrar un enlace al repositorio tampoco a la aplicación desplegada",
-        image: '/public/images/images/handin.jpeg',
+        image: '/images/images/handin.jpeg',
         url: 'https://github.com/LordMathi2741?tab=repositories'
     },
     {
         title: "Aplicación para colaboración entre Artistas y Escritores",
         description: "Desarrollé una aplicación para colaboración entre artistas y escritores como parte del curso de Desarrollo de Aplicaciones Open Source en la UPC. La aplicación incluye como funcionalidades más resaltantes la publicación de libros, interacción entre artistas y escritores y la visualización de portafolios. Utilicé tecnologías como Spring Boot, Angular, Paypal para la gestión de pagos y MYSQL para la persistencia de datos. En el lado del backend se aplicó la arquitectura de CQRS junto con el enfoque de DDD para asegurar la escalabilidad del software de acuerdo al negocio. En el frontend igualmente se aplicó un enfoque bajo DDD para garantizar la escalabilidad de la aplicación web",
-        image: '/public/images/images/artcollab.png',
+        image: '/images/images/artcollab.png',
         url: 'https://github.com/orgs/Open-Source-SW54-Group-3-ArtCollab/repositories'
     }
 ]
